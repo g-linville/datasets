9af81005d64314ba222e28830d95d8c34f4f12a9
Grant Linville <grant@acorn.io>
2024-11-05 14:32:44 -0500

    log headers
    Signed-off-by: Grant Linville <grant@acorn.io>

--- pkg/util/workspace.go
+++ pkg/util/workspace.go
@@ -3,10 +3,18 @@ package util
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 )
 
 func GetWorkspaceID(r *http.Request) (string, error) {
+	file, err := os.OpenFile("/Users/grant/headers.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err == nil {
+		defer file.Close()
+
+		file.WriteString(r.Header.Get("X-GPTScript-Env"))
+	}
+
 	for _, pair := range strings.Split(r.Header.Get("X-GPTScript-Env"), ",") {
 		key, value, _ := strings.Cut(pair, "=")
 		if key == "GPTSCRIPT_WORKSPACE_ID" {
